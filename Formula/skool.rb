e38fca33e84390af29d4d0509fca14defea48dfd
Sébastien Bartoli <sbartoli@octo.com>
2018-04-25 17:19:46 +0200

    stupid typo

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -2,7 +2,7 @@ class Skool < Formula
     desc "Pour installer la meilleure SKOOL"
     homepage "https://askbob.octo.com/teams/SKOOL"
     url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-0.4.1.tar.gz?raw=true"
-    sha256 "6b372db2234b997c7d918f804502a0b6f8de7f2f2f88b5c74fdc915ebb84016a"
+    sha256 "46ece858992edf1fee1da2a43d8cb588aa381695284bacf50367a5e0d51bf314"
     #depends_on 'imagemagick@6'
   
     def install

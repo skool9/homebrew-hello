7c39c200a6f5b392cd6f135dc3864f4182d76dbd
Sébastien Bartoli <sbartoli@octo.com>
2018-04-25 16:07:22 +0200

    formula with dependencies

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -3,8 +3,11 @@ class Skool < Formula
     homepage "https://askbob.octo.com/teams/SKOOL"
     url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-0.3.tar.gz?raw=true"
     sha256 "228524d2c34c04bb1ba8c5fbbcbffd487a395b4720346726fc7dcb2c665f4bbf"
+    depends_on imagemagick@6
   
     def install
+      system "gem install rmagick"
+      system "gem install catpix"
       mkdir "/tmp/skool9-contacts/"
       system "mv ./*.vcf /tmp/skool9-contacts/"
       bin.install "skool"

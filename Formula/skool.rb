779871ddd39aac667d95c98a69f6865ac256b902
Sébastien Bartoli <sbartoli@octo.com>
2018-04-25 16:10:56 +0200

    encapsulated dependency in string

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -3,7 +3,7 @@ class Skool < Formula
     homepage "https://askbob.octo.com/teams/SKOOL"
     url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-0.3.tar.gz?raw=true"
     sha256 "228524d2c34c04bb1ba8c5fbbcbffd487a395b4720346726fc7dcb2c665f4bbf"
-    depends_on imagemagick@6
+    depends_on 'imagemagick@6'
   
     def install
       system "gem install rmagick"

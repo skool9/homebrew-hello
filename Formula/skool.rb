75e178d8763662e0638c448801ea7a15165ead7c
Sébastien Bartoli <sbartoli@octo.com>
2018-04-25 15:46:42 +0200

    try as bin

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -2,12 +2,12 @@ class Skool < Formula
     desc "Pour installer la meilleure SKOOL"
     homepage "https://askbob.octo.com/teams/SKOOL"
     url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-0.3.tar.gz?raw=true"
-    sha256 "8b90bf76deba0b67409201cb6d15cf555b1625fc40cf33599138cd681bdf8909"
+    sha256 "228524d2c34c04bb1ba8c5fbbcbffd487a395b4720346726fc7dcb2c665f4bbf"
   
     def install
       mkdir "/tmp/skool9-contacts/"
       system "mv ./*.vcf /tmp/skool9-contacts/"
-      bin.install "skool.rb"
+      bin.install "skool"
       puts "Vous avez installé la SKOOL9 avec succès ! Tapez `skool` pour démarrer."
     end
   end

70e6a8a8b78cf496b456fdb95a97c7a0b0e47d10
Sébastien Bartoli <sbartoli@octo.com>
2019-02-19 17:29:44 +0100

    bugfix?

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -1,8 +1,8 @@
 class Skool < Formula
     desc "Pour installer la meilleure SKOOL"
     homepage "https://askbob.octo.com/teams/SKOOL"
-    url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-1.0.tar.gz?raw=true"
-    sha256 "3d65a3ec326a2baa31f10550a3e9020fd8e4b0e8adf929b713b197faa67d22a8"
+    url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-1.0.1.tar.gz?raw=true"
+    sha256 "793a30b227a652fa9ca9cf58f0880936e0dc1478b11bdb3947890e5844aaaca4"
     #depends_on 'imagemagick@6'
   
     def install

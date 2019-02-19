14f0ea811d6c151fa23fc44db39060c71645a2fc
Sébastien Bartoli <sbartoli@octo.com>
2019-02-19 19:17:25 +0100

    colors

--- Formula/skool.rb
+++ Formula/skool.rb
@@ -1,8 +1,8 @@
 class Skool < Formula
     desc "Pour installer la meilleure SKOOL"
     homepage "https://askbob.octo.com/teams/SKOOL"
-    url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-1.0.3c.tar.gz?raw=true"
-    sha256 "371091d901fcfc4b7a6c3b52b76a087a075236b826b1d3f2aa6f4de4b94f3c9a"
+    url "https://github.com/skool9/homebrew-hello/blob/master/tarballs/skool-1.0.3d.tar.gz?raw=true"
+    sha256 "5dd89977ea6225de948630b8aa750a97fb75a47abf7877fd749dbde94fa2be76"
     #depends_on 'imagemagick@6'
   
     def install
